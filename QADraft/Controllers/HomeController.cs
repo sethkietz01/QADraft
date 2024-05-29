5ac3b124fa3ce764a356dcaef4c1e4a4a58b7e40
Patrick Wacholtz <patrickwacholtz@gmail.com>
2024-05-29 10:12:46 -0400

    #Finalized User Delete and Edit functionality.

--- QADraft/Controllers/HomeController.cs
+++ QADraft/Controllers/HomeController.cs
@@ -128,19 +128,26 @@ namespace QADraft.Controllers
             {
                 _context.Users.Add(user);
                 _context.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("UserOptions");
             }
             return View(user);
         }
 
         [HttpGet]
-        public IActionResult EditUser(string username, string firstname, string lastname, string email, string role, DateTime createdat)
+        public IActionResult EditUser(string Id)
         {
             if (!IsAuthenticated())
             {
                 return RedirectToAction("Login");
             }
-            return View();
+            int intId = int.Parse(Id);
+            var userData = _context.Users.SingleOrDefault(u => u.Id == intId);
+            if (userData == null)
+            {
+                return View();
+            }
+
+            return View(userData);
         }
 
         [HttpPost]
@@ -150,48 +157,30 @@ namespace QADraft.Controllers
             {
                 return RedirectToAction("Login");
             }
-            user.Password = "Pass";
-
-            if (ModelState.IsValid)
+            else if (ModelState.IsValid)
             {
                 var existingUser = _context.Users.SingleOrDefault(u => u.Username == username);
                 if (existingUser != null)
                 {
                     if (action == "Update User")
                     {
-                        Debug.WriteLine("1");
                         existingUser.Username = user.Username;
-                        Debug.WriteLine("2");
+                        existingUser.Password = user.Password;
                         existingUser.FirstName = user.FirstName;
-                        Debug.WriteLine("3");
                         existingUser.LastName = user.LastName;
-                        Debug.WriteLine("4");
                         existingUser.Email = user.Email;
-                        Debug.WriteLine("5");
                         existingUser.Role = user.Role;
-
-                        Debug.WriteLine("6");
                         _context.SaveChanges();
-                        Debug.WriteLine("7");
-                        return RedirectToAction("UserOptions");
                     }
-                    if (action == "Delete User")
+                    else if (action == "Delete User")
                     {
-                        Debug.WriteLine("DELETE USER");
+                        _context.Remove(existingUser);
+                        _context.SaveChanges();
                     }
                 }
             }
-            if (!ModelState.IsValid)
-            {
-                foreach (var modelStateEntry in ModelState.Values)
-                {
-                    foreach (var error in modelStateEntry.Errors)
-                    {
-                        Debug.WriteLine(error.ErrorMessage);
-                    }
-                }
-            }
-            return View(user);
+
+            return RedirectToAction("UserOptions");
         }
 
         [HttpGet]
@@ -218,33 +207,6 @@ namespace QADraft.Controllers
             return View(model);
         }
 
-        [HttpPost]
-        public IActionResult AddQA(GeekQA model)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.GeekQAs.Add(model);
-                    _context.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Error saving GeekQA");
-                    ModelState.AddModelError(string.Empty, "An error occurred while saving the QA. Please try again.");
-                }
-            }
-
-            // If model state is invalid, repopulate the users list
-            model.Users = _context.Users.Select(u => new SelectListItem
-            {
-                Value = u.Id.ToString(),
-                Text = $"{u.FirstName} {u.LastName}"
-            }).ToList();
-
-            return View(model);
-        }
 
         public IActionResult Links()
         {
@@ -316,31 +278,6 @@ namespace QADraft.Controllers
                 .Where(q => q.Severity >= 10) // Example condition for flagged accounts
                 .ToList();
             return PartialView("_FlaggedAccounts", flaggedAccounts);
-        }
-
-        // Updated AddQA actions to return full views
-        [HttpGet]
-        public IActionResult AddQA()
-        {
-            if (!IsAuthenticated())
-            {
-                return RedirectToAction("Login");
-            }
-
-            var users = _context.Users.Select(u => new SelectListItem
-            {
-                Value = u.Id.ToString(),
-                Text = $"{u.FirstName} {u.LastName}"
-            }).ToList();
-
-            GeekQA model = new GeekQA
-            {
-                ErrorDate = DateTime.Now,
-                FoundOn = DateTime.Now,
-                Users = users
-            };
-
-            return View(model);
         }
 
         [HttpPost]
