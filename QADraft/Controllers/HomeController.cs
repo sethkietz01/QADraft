7c6333a1de03dc035c3cfb33347e82ac8e71bebd
danieltaylor2432 <danieltaylor234211@gmail.com>
2024-05-29 15:01:35 -0400

    test

--- QADraft/Controllers/HomeController.cs
+++ QADraft/Controllers/HomeController.cs
@@ -47,8 +47,9 @@ namespace QADraft.Controllers
         [HttpPost]
         public IActionResult Login(string username, string password)
         {
-            var user = _context.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
-            if (user != null)
+            var user = _context.Users.SingleOrDefault(u => u.Username == username);
+
+            if (user != null && user.Password == password)
             {
                 HttpContext.Session.SetString("IsAuthenticated", "true");
                 HttpContext.Session.SetString("username", username);
@@ -60,6 +61,8 @@ namespace QADraft.Controllers
             ViewBag.Message = "Invalid login attempt.";
             return View();
         }
+
+
 
         [HttpGet]
         public IActionResult Logout()
