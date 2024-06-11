fdb920768b8672ab8a38f5534bd4f340f7f44c1e
danieltaylor2432 <danieltaylor234211@gmail.com>
2024-06-11 08:44:05 -0400

    testing

---
+++ QADraft/Controllers/ChartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QADraft.Models;
+using QADraft.Services;
+using System;
+using System.Collections.Generic;
+
+namespace QADraft.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChartController : ControllerBase
+    {
+        private readonly GeekQAService _geekQAService;
+
+        public ChartController(GeekQAService geekQAService)
+        {
+            _geekQAService = geekQAService;
+        }
+
+        [HttpGet]
+        public IActionResult GetFilteredQAs([FromQuery] DateTime? startDate, [FromQuery] DateTime? endDate, [FromQuery] string category)
+        {
+            var data = _geekQAService.GetFilteredQAs(startDate, endDate, category);
+            return Ok(data);
+        }
+    }
+}
