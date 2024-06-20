616d9ac1e6122faf1aebb3cee6206102e91cbae8
Patrick Wacholtz <patrickwacholtz@gmail.com>
2024-06-20 13:57:43 -0400

    #Partially Commented QAController.

--- QADraft/Controllers/QAController.cs
+++ QADraft/Controllers/QAController.cs
@@ -57,10 +57,16 @@ namespace QADraft.Controllers
             return View(model);
         }
 
-        // Process the AddQA POST
+        // Process the AddQA POST request
         [HttpPost]
         public IActionResult AddQA(GeekQA model)
         {
+            // Verify that the user is logged in, if not direct them to login page
+            if (!SessionUtil.IsAuthenticated(HttpContext))
+            {
+                return RedirectToAction("Login");
+            }
+
             // Verify that the submitted model state is valid (checks types and if all required model attributes are there)
             if (ModelState.IsValid)
             {
@@ -115,6 +121,12 @@ namespace QADraft.Controllers
         [HttpGet]
         public async Task<IActionResult> Filter(string dateFilter, DateTime? startDate, DateTime? endDate, int? committedBy, int? loggedBy, string category)
         {
+            // Verify that the user is logged in, if not direct them to login page
+            if (!SessionUtil.IsAuthenticated(HttpContext))
+            {
+                return RedirectToAction("Login");
+            }
+
             // Pass all users into the viewbag to populate drop-down selectors
             ViewBag.Users = _context.Users.ToList();
             // Pass all error categories into viewbag
@@ -127,208 +139,225 @@ namespace QADraft.Controllers
                 new SelectListItem { Value = "Other", Text = "Other" }
             };
 
-            // Fetch all QAs
+            // Fetch all QAs as queryable
             var qas = _context.GeekQAs.AsQueryable();
 
+            // Check if the datefilter box is checked
             if (!string.IsNullOrEmpty(dateFilter))
             {
+                // Check if there is both a start and end date entered
                 if (startDate.HasValue && endDate.HasValue)
                 {
                     // Both start date and end date are provided
                     qas = qas.Where(q => q.ErrorDate >= startDate.Value && q.ErrorDate <= endDate.Value);
                 }
+                // If not, check for a lone start date
                 else if (startDate.HasValue)
                 {
                     // Only start date is provided, filter from start date to current date
                     qas = qas.Where(q => q.ErrorDate >= startDate.Value && q.ErrorDate <= DateTime.Now);
                 }
+                // Finally, check for a lone end date
                 else if (endDate.HasValue)
                 {
-                    // Only end date is provided, filter from the minimum date to current date
+                    // Only end date is provided, filter from the minimum date to end date
                     qas = qas.Where(q => q.ErrorDate >= DateTime.MinValue && q.ErrorDate <= endDate.Value);
                 }
             }
-
+            // Check if the Commited By field has been given a value
             if (committedBy.HasValue)
             {
+                // Add QAs that were commited by the selected person
                 qas = qas.Where(q => q.CommittedById == committedBy.Value);
             }
 
+            // Check if the Logged By field has been given a value
             if (loggedBy.HasValue)
             {
+                // Add QAs that were logged by the selected person
                 qas = qas.Where(q => q.FoundById == loggedBy.Value);
             }
 
+            // Check if the Category field was given a value
             if (!string.IsNullOrEmpty(category))
             {
+                // Add QAs that are of the same category
                 qas = qas.Where(q => q.CategoryOfError == category);
             }
 
+            // Add the QAs to a list and await async
             var model = await qas.ToListAsync();
 
+            // Insert QAs into Viewbag as the inital content
             ViewBag.InitialContent = await RenderViewAsync("_Filter", model);
+            // Return the view with the QAs
             return PartialView("_Filter", model);
         }
 
-
-
+        // Display the QAs of the logged in user
         [HttpGet]
         public IActionResult YourQAs()
         {
+            // Verify that the user is logged in, if not direct them to login page
             if (!SessionUtil.IsAuthenticated(HttpContext))
             {
                 return RedirectToAction("Login");
             }
-            var id = SessionUtil.GetId(HttpContext);
-            var user = _context.Users.SingleOrDefault(u => u.Id == id);
-            var qa = _context.GeekQAs.Where(q => q.CommittedById == user.Id).Include(q => q.FoundBy).ToList();
 
-            return View(qa);
+            // Get the ID of the logged in user
+            var id = SessionUtil.GetId(HttpContext);
+            // Find the matching ID in the database table Users
+            var user = _context.Users.SingleOrDefault(u => u.Id == id);
+            // Check if a user was found
+            if (user != null)
+            {
+                // If so, find all QAs where the ID matches
+                var qa = _context.GeekQAs.Where(q => q.CommittedById == user.Id).Include(q => q.FoundBy).ToList();
+                return View(qa);
+            }
+            // Return the view with the QAs
+            return View();
+            
         }
 
-
-
+        // Display all of the Geek QAs
         [HttpGet]
         public IActionResult AllGeekQAs()
         {
+            // Verify that the user is logged in, if not direct them to login page
             if (!SessionUtil.IsAuthenticated(HttpContext))
             {
                 return RedirectToAction("Login");
             }
+
+            // Fetch all QAs along with the attributes CommitedBy and FoundBy (These attributes are names while CommitedByID and FoundByID are IDs).
             var qas = _context.GeekQAs
                 .Include(q => q.CommittedBy)
                 .Include(q => q.FoundBy)
                 .ToList();
 
-            //code to get data for piechart
-            Console.WriteLine("Call PieChart");
+            // Get dictionaries of all QA Categories/Nature in form {Name:Count}. Pass these Dicts into the ViewBag.
             ViewBag.categoryDict = GetQADict("category");
             ViewBag.natureDict = GetQADict("nature");
 
+            // Return view with ViewBag and list of QAs
             return View("_AllGeekQAs", qas);
         }
 
 
 
-
+        // Display the FlaggedAccounts page
         [HttpGet]
         public IActionResult FlaggedAccounts()
         {
+            // Verify that the user is logged in, if not direct them to login page
             if (!SessionUtil.IsAuthenticated(HttpContext))
             {
                 return RedirectToAction("Login");
             }
 
+            // Fetch all of QAs along with the attributes CommitedBy and FoundBy
             var flaggedAccounts = _context.GeekQAs
                 .Include(q => q.CommittedBy)
                 .Include(q => q.FoundBy)
-                .Where(q => q.Severity >= 10) // Example condition for flagged accounts
+                .Where(q => q.Severity >= 10) // Only *select* those QAs where the severity is 10 (or greater)
                 .ToList();
+
+            // Return the view with the list of flagged accounts
             return PartialView("_FlaggedAccounts", flaggedAccounts);
         }
 
 
 
-
+        // Display the QADescriptions page
         [HttpGet]
         public IActionResult QADescriptions()
         {
+            // Verify that the user is logged in, if not direct them to login page
             if (!SessionUtil.IsAuthenticated(HttpContext))
             {
                 return RedirectToAction("Login");
             }
+
+            // Return the view
             return View("_QADescriptions");
         }
 
-
-
-
-
+        // Management function for View/Edit and Delete QA
         [HttpPost]
         public IActionResult ManageQA(int qaId, string action, string source)
         {
-            Debug.WriteLine("ManagQA reached");
-            Debug.WriteLine("Action: ", action);
-            if (action == "View/Edit")
-            {
-                Debug.WriteLine(qaId);
-                Debug.WriteLine("Edit QA");
-                return RedirectToAction("EditQA", new { id = qaId });
-            }
-            else if (action == "Delete QA")
-            {
-                Debug.WriteLine("Delete QA reached");
-                return RedirectToAction("DeleteQA", new { id = qaId, src = source });
-            }
-
-            return RedirectToAction("Index"); // Default redirection
-        }
-
-        [HttpGet]
-        public IActionResult ViewQA(int id)
-        {
+            // Verify that the user is logged in, if not direct them to login page
             if (!SessionUtil.IsAuthenticated(HttpContext))
             {
                 return RedirectToAction("Login");
             }
 
-            var qa = _context.GeekQAs.Find(id);
-            if (qa == null)
+            // Check if the passed action is to View/ Edit
+            if (action == "View/Edit")
             {
-                return NotFound();
+                // Direct to the EditQA action and pass the ID of the QA and the source page
+                return RedirectToAction("EditQA", new { id = qaId, src = source });
+            }
+            // Check if the passed action is to Delete
+            else if (action == "Delete QA")
+            {
+                // Direct to the DeleteQA action and pass the ID of the QA and the source page
+                return RedirectToAction("DeleteQA", new { id = qaId });
             }
 
-            var users = _context.Users.Select(u => new SelectListItem
-            {
-                Value = u.Id.ToString(),
-                Text = $"{u.FirstName} {u.LastName}"
-            }).ToList();
-
-            qa.Users = users;
-
-            Console.WriteLine("QA:" + qa.Id);
-            Console.WriteLine(qa.Users);
-            Console.WriteLine(qa.CategoryOfError);
-            Console.WriteLine(qa.NatureOfError);
-
-            return View(qa);
+            // Otherwise, an error has occured and they will be redirected to the home page (Home/Index)
+            return RedirectToAction("Index", "Home");
         }
 
-
+        // Display the Edit(View)QA page
+        // View and Edit QA are consolidated into the same page
         [HttpGet]
         public IActionResult EditQA(int id)
         {
-            Debug.WriteLine("id:");
-            Debug.WriteLine(id);
+            // Verify that the user is logged in, if not direct them to login page
             if (!SessionUtil.IsAuthenticated(HttpContext))
             {
                 return RedirectToAction("Login");
             }
 
+            // Fetch the QA that matches the ID of the QA selected from table
             var qa = _context.GeekQAs.Find(id);
-            if (qa == null)
+            // Verify that the QA was found and exists
+            if (qa != null)
             {
-                return NotFound();
+                // Fetch all of the users' names and store as a list
+                var users = _context.Users.Select(u => new SelectListItem
+                {
+                    Value = u.Id.ToString(),
+                    Text = $"{u.FirstName} {u.LastName}"
+                }).ToList();
+
+                // Verify that users were found
+                if (users != null)
+                {
+                    // Assign the list of users to qa.Users and return the view with the fetched QA
+                    qa.Users = users;
+                    return View(qa);
+                }
             }
-
-            var users = _context.Users.Select(u => new SelectListItem
-            {
-                Value = u.Id.ToString(),
-                Text = $"{u.FirstName} {u.LastName}"
-            }).ToList();
-
-            qa.Users = users;
-            return View(qa);
+            // If the qa or users were not found (null), return the default view.
+            return View();
         }
 
+        // Process the EditQA POST request
         [HttpPost]
-        public IActionResult EditQA(GeekQA model)
+        public IActionResult EditQA(GeekQA model, string src)
         {
+            Debug.WriteLine($"editsrc:{src}");
+            // Verify that the passed model is valid and contains all required fields
             if (ModelState.IsValid)
             {
                 try
                 {
+                    // Fetch the QA that matches the ID of the QA passed during POST
                     var qa = _context.GeekQAs.Find(model.Id);
+
                     if (qa == null)
                     {
                         return NotFound();
@@ -347,9 +376,12 @@ namespace QADraft.Controllers
 
                     _context.Update(qa);
                     _context.SaveChanges();
-                    return RedirectToAction("AllGeekQAs");
+                    if (src == "Filter")
+                        return RedirectToAction("Filter");
+                    else if (src == "AllGeekQAs")
+                        return RedirectToAction("AllGeekQAs");
                 }
-                catch (Exception ex)
+                catch
                 {
                     ModelState.AddModelError(string.Empty, "An error occurred while updating the QA. Please try again.");
                 }
@@ -366,7 +398,7 @@ namespace QADraft.Controllers
 
 
         [HttpGet]
-        public IActionResult DeleteQA(int id, string src)
+        public IActionResult DeleteQA(int id)
         {
             if (!SessionUtil.IsAuthenticated(HttpContext))
             {
@@ -378,10 +410,8 @@ namespace QADraft.Controllers
                 _context.Remove(findQA);
                 _context.SaveChanges();
             }
-            if (src == "Filter")
-                return RedirectToAction("Filter");
 
-            return RedirectToAction("AllGeekQAs");
+            return View();
         }
 
 
